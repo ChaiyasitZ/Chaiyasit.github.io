8e6dc37139f3bfa82b32536692e5bdd99a4a5a5e
ChaiyasitZ <s6506022610014@email.kmutnb.ac.th>
2022-11-02 05:24:22 +0700

    Lastest

---
+++ python/DATA types, Variables AND Expression.py
@@ -0,0 +1,42 @@
+# VARIABLES
+
+# print("Hello, World!")
+
+
+# name = "Chaiyasit" # String
+# age = 19 # Integer
+
+# Psedocode
+
+# weight = float(input("Enter your weight kg: "))
+# height = float(input("Enter your height cm: "))
+# bmi = weight / (height/100)**2
+# print(f"You BMI is {bmi:.2f}")
+
+# num1 = str(4) # num1 will be '4'
+# num2 = int(4) # num2 will be 4
+# num3 = float(4) # num3 will be 4.0
+
+# name = "Chaiyasit"
+# name = 'Chaiyasit'
+# # both is the same
+
+# Num = 4
+# num = "Four"
+# # num will not overwrite Num
+
+myname = 'Chaiyasit'
+my_name = 'Chaiyasit'
+_my_name = 'Chaiyasit'
+myName = 'Chaiyasit'
+MYNAME = 'Chaiyasit'
+myname1 = 'Chaiyasit'
+
+print(myname)
+print(my_name)
+print(_my_name)
+print(myName)
+print(MYNAME)
+print(myname1)
+
+
