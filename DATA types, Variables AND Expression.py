8ef1f871759a04caf4de5bf1552ef7ca2e707ab0
ChaiyasitZ <s6506022610014@email.kmutnb.ac.th>
2022-11-03 08:59:11 +0700

    lastest

--- DATA types, Variables AND Expression.py
+++ DATA types, Variables AND Expression.py
@@ -25,18 +25,415 @@
 # num = "Four"
 # # num will not overwrite Num
 
-myname = 'Chaiyasit'
-my_name = 'Chaiyasit'
-_my_name = 'Chaiyasit'
-myName = 'Chaiyasit'
-MYNAME = 'Chaiyasit'
-myname1 = 'Chaiyasit'
+# myname = 'Chaiyasit'
+# my_name = 'Chaiyasit'
+# _my_name = 'Chaiyasit'
+# myName = 'Chaiyasit'
+# MYNAME = 'Chaiyasit'
+# myname1 = 'Chaiyasit'
 
-print(myname)
-print(my_name)
-print(_my_name)
-print(myName)
-print(MYNAME)
-print(myname1)
+# print(myname)
+# print(my_name)
+# print(_my_name)
+# print(myName)
+# print(MYNAME)
+# print(myname1)
+
+# 1myname = 'Chaiyasit'
+# my-name = 'Chaiyasit'
+# my name = 'Chaiyasit'
+
+# print(1myname)
+# print(my-name)
+# print(my name)
+
+# x = "Hello World"	                #str	
+# x = 20	                            #int	
+# x = 20.5	                        #float	
+# x = 1j	                            #complex	
+# x = ["cat", "dog"]	                #list	
+# x = ("cat", "dog")	                #tuple	
+# x = range(11)	                    #range	
+# x = {"name" : "New", "age" : 19}	#dict	
+# x = {"cat", "dog",}             	#set	
+# x = frozenset({"cat", "dog"})       #frozenset	
+# x = True	                        #bool	
+# x = b"Hello"	                    #bytes	
+# x = bytearray(5)	                #bytearray	
+# x = memoryview(bytes(5))	        #memoryview	
+# x = None	                        #NoneType
+
+# myFirstName   = "Chaiyasit" 
+# # Camel Case, Each word, except the first, starts with a capital letter
+# MyFirstName   = "Chaiyasit" 
+# # Pascal Case, Each word starts with a capital letter
+# my_first_name = "Chaiyasit" 
+# # Snake Case, Each word is separated by an underscore character
+
+# animal1, animal2, animal3 = "Lion", "Leopard", "Tiger"
+# # Assign multiple value to multiple variables
+
+# reversed_word =  ['and', 'as', 'asset', 'break', 'class', 'continue', 'def', 'del', 'elif'
+#                   'else', 'except', 'finally', 'FALSE', 'for', 'from', 'global', 'if', 'import' 
+#                   'in', 'is', 'lambda', 'nonlocal', 'None', 'not', 'or' ,'passs', 'raise', 'return'
+#                   'TRUE', 'try', 'with', 'while', 'yield']
+# # Not recommended to use word in list to assign variable
+
+# stand = True 
+# sit = False 
 
 
+# pet1 = 'cat'
+# pet2 = "cat"
+# # pet1 and pet2 is the same
+
+# str1, str2 , str3 = "Hello", "World", ""
+
+# letter = 'She said, "That is a great tasting mango!" '
+# letter = " That's a great tasting mango! "
+
+# y = x^^2 + 2x + 1
+# # y, x ,1 is operand
+# # ^, +, = is operator
+# # y = x^2 + 2x + 1 is expression
+
+# # Operator
+# + Add two operands or unary plus                                     example	x + y
+# - Subtract right operand from the left or unary minus                example	x - y
+# * Multiply two operands                                              example	x * y
+# / Divide left operand by the right one                               example	x / y
+# % Modulus - remainder of the division of left operand by the right   example	x % y
+# // Floor division - division that results into whole number adjusted
+# to the left in the number line                                       example    x // y
+# ** Exponent - left operand raised to the power of right              example	x ** y
+
+# # Operator Precedence
+# # Piority
+# 1. ()
+# 2. **
+# 3. *, /, %, //
+# 4. +, -
+# 5. <=, <, >, >=
+# 6. ==, !=
+# 7. =
+
+# # Operator
+# > Greater that - True if left operand is greater than the right                            example  x > y
+# < Less that - True if left operand is less than the right                                  example	x < y
+# >= Greater than or equal to - True if left operand is greater than or equal to the right   example	x >= y
+# <= Less than or equal to - True if left operand is less than or equal to the right         example	x <= y
+# == Equal to - True if both operands are equal                                              example	x == y
+# != Not equal to - True if operands are not equal                                           example	x != y
+
+# # Operator
+# AND True if both the operands are true         example	x and y
+# OR True if either of the operands is true      example	x or y
+# NOT True if operand is false                   example	not x
+
+# # Operator
+# &  Bitwise AND          example	x & y   = 0 (0000 0000)
+# |  Bitwise OR           example	x | y   = 14 (0000 1110)
+# ~  Bitwise NOT          example	~x      = -11 (1111 0101)
+# ^  Bitwise XOR          example	x ^ y   = 14 (0000 1110)
+# >> Bitwise right shift  example	x >> 2  = 2 (0000 0010)
+# << Bitwise left shift   example	x << 2  = 40 (0010 1000)
+
+# # Operatpr
+# =   example x = 3   equivalent to x = 3
+# +=  example x += 3  equivalent to x = x + 3
+# -=  example x -= 3  equivalent to x = x - 3
+# *=  example x *= 3  equivalent to x = x * 3
+# /=  example x /= 3  equivalent to x = x / 3
+# %=  example x %= 3  equivalent to x = x % 3
+# //= example x //= 3 equivalent to x = x // 3
+# **= example x **= 3 equivalent to x = x ** 3
+# &=  example x &= 3  equivalent to x = x & 3
+# |=  example x |= 3  equivalent to x = x | 3
+# ^=  example x ^= 3  equivalent to x = x ^ 3
+# >>= example x >>= 3 equivalent to x = x >> 3
+# <<= example x <<= 3 equivalent to x = x << 3
+
+# # Operator
+# is      True if the operands are identical      example x is True
+# is not  True if the operands are not identical  example x is not True
+
+# # Operator
+# in      True if value/variable is found in the sequence      example 5 in x
+# not in  True if value/variable is not found in the sequence  example 5 not in x
+
+# name   = str(input('Enter your name?: '))        # input() function always return string
+# weight = float(input('Enter your weight?: '))    # input() function always return float
+# age    = int(input('Enter your age?: '))         # input() function always return int
+# print(name, weight, age)
+# '''
+#     This comment python interpreter will ignore
+#     All the text here
+# '''
+# # This also comment
+
+# if Condition :
+#     Statement
+#     Statement
+#     etc.
+# elif:
+#     Statement
+#     Statement
+#     etc.
+
+# # # All of the statements in the block are indented
+
+# if sales > 50000:
+#     bonus = 500.0
+#     commission_rate = 0.12
+#     print('You met your sales quota!')
+
+# score = int(input('Enter your score: '))
+# if score >= 50:
+#     print('You passed the exam.')
+# else:
+#     print('You failed the exam.')
+    
+# '''
+# Enter your score: 50
+# Your passed the exam.
+# '''
+
+# s1 = 'Hello'
+# s2 = 'World!'
+# if s1 > s2:
+#     print(s2)
+#     print(s1)
+# else:
+#     print(s1)
+#     print(s2)
+
+# num = float(input('Enter a number: '))
+# if num > 0:
+#     print('The number is positive.')
+# elif num == 0:
+#     print('The number is zero.')
+# else:
+#     print('The number is negative.')
+    
+# '''
+# Enter a number: 5
+# The number is positive.
+# '''
+
+# inchar = input('Enter a character: ')
+# if inchar >= 'A' and inchar <= 'Z':
+#     print('You entered an uppercase letter.', inchar)
+# elif inchar >= 'a' and inchar <= 'z':
+#     print('You entered a lowercase letter.', inchar)
+# elif inchar >= '0' and inchar <= '9':
+#     print('You entered a digit.', inchar)
+# else:
+#     print('You entered a special character.', inchar)
+
+# '''
+# Enter a character: @
+# You entered a special character. @
+# '''
+
+# num = float(input('Enter a number: '))
+# if num > 0:
+#     if num == 0:
+#         print('The number is zero.')
+#     else:
+#         print('The number is positive.')
+# else:
+#     print('The number is negative.')
+    
+# '''
+# Enter a number: 5
+# This number is positive.
+# ''' 
+
+# sales = float(input('Enter the amount of sales: '))
+# comm_rate = float(input('Enter the commission rate: '))
+# comission = sales * comm_rate
+# print('The commission is $', format(comission, ',.2f'))
+
+# sales = float(input('Enter the amount of sales: '))
+# comm_rate = float(input('Enter the commission rate: '))
+# comission = sales * comm_rate
+# print('The commission is $', format(comission, ',.2f'))
+
+# sales = float(input('Enter the amount of sales: '))
+# comm_rate = float(input('Enter the commission rate: '))
+# comission = sales * comm_rate
+# print('The commission is $', format(comission, ',.2f'))
+
+# while Condition:
+#     Statement
+#     Statement 
+#     ...
+#     Statement 
+
+# count = 0
+# while (count < 3):
+#     count = count + 1 
+#     print('Hello World')
+
+# # All the statements in the block are indented
+
+# for variable in [value1, value2, etc.]:
+#     statement
+#     statement
+#     etc.
+
+# # All the statements in the block are indented
+
+# print('I will display the numbers 1 through 5.')
+# for num in [1, 2, 3, 4, 5]:
+#     print(num)
+
+# max = 5 
+# total = 0.0
+# print('This program calculates the sum of')
+# print(max, 'numbers you will enter.')
+
+# for counter in range(max):
+#     number = float(input('Enter a number: '))
+#     total = total + number
+# print('The total is', total)
+
+# numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+# sum = 0
+# for val in numbers :
+#     sum = sum+val
+#     print(sum)
+# print("The sum is", sum)
+
+# score = int(input('Enter your test score: '))
+# while score < 0 or score > 100:
+#     print('Invalid score. The score must be from 0 through 100.')
+#     print('or greather than 100')
+#     score = int(input('Enter the correct score: '))
+
+# for hours in range(24):
+#     for minutes in range(60):
+#         for seconds in range(60):
+#             print(hours, ':', minutes, ':', seconds)
+
+# # Pass
+# for letter in 'Chaiyasit Mintakorn':
+#     if letter == 'i' or letter == 't':
+#         pass
+#     print('Current Letter :', letter)
+    
+# # Continue
+# for letter in 'Chaiyasit Mintakorn':
+#     if letter == 'i' or letter == 't':
+#         continue
+#     print('Current Letter :', letter)
+    
+# # Break
+# for letter in 'Chaiyasit Mintakorn':
+#     if letter == 'i' or letter == 't':
+#         break
+#     print('Current Letter :', letter)
+
+# x = 'Love'
+# x = x + ' You'
+
+# ''' x = Love You '''
+
+# INE = 'Information and Network Engineering'
+# INE_len = len(INE)
+# print(INE_len)
+# ''' len = 35 '''
+
+# city = 'Bangkok'
+# index = 0
+# while index < len(city):
+#     print(city[index])
+#     index += 1
+
+
+# # Lowercase
+# fruit = 'Apple'
+# print(fruit.lower()) 'fruit = apple'
+
+# # Uppercase
+# fruit = 'Apple'
+# print(fruit.upper()) 'fruit = APPLE'
+
+# # Format
+# one = 'I'
+# two = 'Love'
+# three = 'Python'
+# print('{} {} {}'.format(one, two, three))
+
+# ballon = '   Chaiyasit has a balloon    '
+# print('[', ballon.strip(), ']')
+# print('[', ballon.lstrip(), ']')
+# print('[', ballon.rstrip(), ']')
+
+# print(ballon.find('haiya'))
+# print(ballon.replace('has', 'had'))
+
+
+# # String Methods
+# strip()  Removes any whitespace from the beginning or the end 
+# lstrip() Removes any whitespace from the beginning
+# rstrip() Removes any whitespace from the end
+
+# endswith()   Returns True if the string ends with the specified value
+# find()       Searches the string for a specified value and returns the position of where it was found
+# replace()    Returns a string where a specified value is replaced with a specified value.
+# startswith() Returns True if the string starts with the specified value
+
+# def function_name():
+#     Statement
+#     Statement
+#     etc.
+#     return expression
+
+
+# # First we define the main function 
+# def main():
+#     print('I have a message for you.')
+#     message()
+#     print('Goodbye!')
+
+# # Next we define the message function
+# def message():
+#     print('I am Chaiyasit Mintakorn')
+#     print('I Love Python')
+
+# # Call the main function
+# main()
+
+# def main():
+#     texas()
+#     california()
+
+# def texas():
+#     birds = 5000
+#     print('texas has', birds, 'birds.')
+
+# def california():
+#     birds = 8000
+#     print('california has', birds, 'birds.')
+    
+# main()
+
+# def main():
+#     value = 5
+#     show_double(value)
+
+# def show_double(number):
+#     result = number * 2
+#     print(result)
+
+# main()
+
+# File Mode
+'r' Read mode which is used when the file is only being read
+'w' Write mode which is used to edit and write new information to the file
+'x' Create mode which is used to create files, returns an error if the file exist
+'a' Append mode, which is used to add new data to the end of the file
+'t' Text mode which is used when working with text files
+'b' Binary mode which is used when working with binary files
+'+' Read and write
